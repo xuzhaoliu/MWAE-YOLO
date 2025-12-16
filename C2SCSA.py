8af5e69a8617da053b91170d423ed3f3b76bcfdf
xuzhaoliu <156508270+xuzhaoliu@users.noreply.github.com>
2025-12-16 10:41:24 +0800

    C2SCSA model

---
+++ C2SCSA.py
@@ -0,0 +1,182 @@
+import typing as t
+import torch
+import torch.nn as nn
+from einops import rearrange
+from mmengine.model import BaseModule
+
+__all__ = ['SCSA', 'C2SCSA']
+
+class SCSA(BaseModule):
+    def __init__(
+            self,
+            dim: int,
+            head_num: int = 4,
+            window_size: int = 7,
+            group_kernel_sizes: t.List[int] = [3, 5, 7, 9],
+            qkv_bias: bool = False,
+            fuse_bn: bool = False,
+            norm_cfg: t.Dict = dict(type='BN'),
+            act_cfg: t.Dict = dict(type='ReLU'),
+            down_sample_mode: str = 'avg_pool',
+            attn_drop_ratio: float = 0.,
+            gate_layer: str = 'sigmoid',
+    ):
+        super(SCSA, self).__init__()
+        self.dim = dim
+        head_num = dim // 64
+        if head_num == 0:
+            head_num = 1
+        self.head_num = head_num
+        self.head_dim = dim // head_num
+        self.scaler = self.head_dim ** -0.5
+        self.group_kernel_sizes = group_kernel_sizes
+        self.window_size = window_size
+        self.qkv_bias = qkv_bias
+        self.fuse_bn = fuse_bn
+        self.down_sample_mode = down_sample_mode
+        assert self.dim % 4 == 0, 'The dimension of input feature should be divisible by 4.'
+        self.group_chans = group_chans = self.dim // 4
+
+        self.local_dwc = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[0],
+                                   padding=group_kernel_sizes[0] // 2, groups=group_chans)
+        self.global_dwc_s = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[1],
+                                      padding=group_kernel_sizes[1] // 2, groups=group_chans)
+        self.global_dwc_m = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[2],
+                                      padding=group_kernel_sizes[2] // 2, groups=group_chans)
+        self.global_dwc_l = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[3],
+                                      padding=group_kernel_sizes[3] // 2, groups=group_chans)
+        
+        self.sa_gate = nn.Softmax(dim=2) if gate_layer == 'softmax' else nn.Sigmoid()
+        self.norm_h = nn.GroupNorm(4, dim)
+        self.norm_w = nn.GroupNorm(4, dim)
+
+        self.conv_d = nn.Identity()
+        self.norm = nn.GroupNorm(1, dim)
+        
+        self.q = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        self.k = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        self.v = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
+        
+        self.attn_drop = nn.Dropout(attn_drop_ratio)
+        self.ca_gate = nn.Softmax(dim=1) if gate_layer == 'softmax' else nn.Sigmoid()
+
+        if window_size == -1:
+            self.down_func = nn.AdaptiveAvgPool2d((1, 1))
+        else:
+            if down_sample_mode == 'recombination':
+                self.down_func = self.space_to_chans
+                self.conv_d = nn.Conv2d(in_channels=dim * window_size ** 2, out_channels=dim, kernel_size=1, bias=False)
+            elif down_sample_mode == 'avg_pool':
+                self.down_func = nn.AvgPool2d(kernel_size=(window_size, window_size), stride=window_size)
+            elif down_sample_mode == 'max_pool':
+                self.down_func = nn.MaxPool2d(kernel_size=(window_size, window_size), stride=window_size)
+    
+    def space_to_chans(self, x):
+        B, C, H, W = x.shape
+        x = x.view(B, C, H // self.window_size, self.window_size, W // self.window_size, self.window_size)
+        x = x.permute(0, 1, 3, 5, 2, 4).contiguous()
+        x = x.view(B, C * self.window_size * self.window_size, H // self.window_size, W // self.window_size)
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, h_, w_ = x.size()
+        x_h = x.mean(dim=3)
+        l_x_h, g_x_h_s, g_x_h_m, g_x_h_l = torch.split(x_h, self.group_chans, dim=1)
+        x_w = x.mean(dim=2)
+        l_x_w, g_x_w_s, g_x_w_m, g_x_w_l = torch.split(x_w, self.group_chans, dim=1)
+        x_h_attn = self.sa_gate(self.norm_h(torch.cat((
+            self.local_dwc(l_x_h),
+            self.global_dwc_s(g_x_h_s),
+            self.global_dwc_m(g_x_h_m),
+            self.global_dwc_l(g_x_h_l),
+        ), dim=1)))
+
+        x_h_attn = x_h_attn.view(b, c, h_, 1)
+
+        x_w_attn = self.sa_gate(self.norm_w(torch.cat((
+            self.local_dwc(l_x_w),
+            self.global_dwc_s(g_x_w_s),
+            self.global_dwc_m(g_x_w_m),
+            self.global_dwc_l(g_x_w_l)
+        ), dim=1)))
+        x_w_attn = x_w_attn.view(b, c, 1, w_)
+
+        x = x * x_h_attn * x_w_attn
+
+        y = self.down_func(x)
+        y = self.conv_d(y)
+        _, _, h_, w_ = y.size()
+
+        y = self.norm(y)
+        q = self.q(y)
+        k = self.k(y)
+        v = self.v(y)
+        
+        q = rearrange(q, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', 
+                      head_num=int(self.head_num), head_dim=int(self.head_dim))
+        k = rearrange(k, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', 
+                      head_num=int(self.head_num), head_dim=int(self.head_dim))
+        v = rearrange(v, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', 
+                      head_num=int(self.head_num), head_dim=int(self.head_dim))
+
+        attn = q @ k.transpose(-2, -1) * self.scaler
+        attn = self.attn_drop(attn.softmax(dim=-1))
+        
+        attn = attn @ v
+        
+        attn = rearrange(attn, 'b head_num head_dim (h w) -> b (head_num head_dim) h w', 
+                        h=int(h_), w=int(w_))
+        attn = attn.mean((2, 3), keepdim=True)
+        attn = self.ca_gate(attn)
+        return attn * x
+
+
+
+def autopad(k, p=None, d=1):
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]
+    return p
+
+
+class Conv(nn.Module):
+    default_act = nn.SiLU()
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
+class PSABlock(nn.Module):
+    def __init__(self, c, attn_ratio=0.5, num_heads=4, shortcut=True) -> None:
+        super().__init__()
+        self.attn = SCSA(c)
+        self.ffn = nn.Sequential(Conv(c, c * 2, 1), Conv(c * 2, c, 1, act=False))
+        self.add = shortcut
+
+    def forward(self, x):
+        x = x + self.attn(x) if self.add else self.attn(x)
+        x = x + self.ffn(x) if self.add else self.ffn(x)
+        return x
+
+
+class C2SCSA(nn.Module):
+    def __init__(self, c1, c2, n=1, e=0.5):
+        super().__init__()
+        assert c1 == c2,
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+        self.m = nn.Sequential(*(PSABlock(self.c, attn_ratio=0.5, num_heads=self.c // 64) for _ in range(n)))
+
+    def forward(self, x):
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = self.m(b)
+        return self.cv2(torch.cat((a, b), 1))
